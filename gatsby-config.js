b1cdc91360037200f2b1c2e79f048de76b82f988
Mark Lewis <technical@marketingcraze.com>
2023-09-19 13:53:38 +0100

    commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,6 +39,7 @@ if (
         `siteUrl can't be localhost and needs to be configured in siteConfig. Check the README.`
     ); // eslint-disable-line
 }
+plugins: [`gatsby-plugin-netlify`]
 
 /**
  * This is the place where you can tell Gatsby which plugins to use
