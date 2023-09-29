2008cbbe57c95d37bb2f013c2e91c8afb42e5f06
Mark Lewis <technical@marketingcraze.com>
2023-09-29 14:23:50 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,240 +1,237 @@
-/* eslint-disable semi */
-/* eslint-disable no-unsafe-finally */
-/* eslint-disable no-restricted-syntax */
-const path = require(`path`);
+    const path = require(`path`);
 
-const config = require(`./src/utils/siteConfig`);
-const generateRSSFeed = require(`./src/utils/rss/generate-feed`);
+    const config = require(`./src/utils/siteConfig`);
+    const generateRSSFeed = require(`./src/utils/rss/generate-feed`);
 
-let ghostConfig;
+    let ghostConfig;
 
-try {
-    ghostConfig = require(`./.ghost`);
-} catch (e) {
-    ghostConfig = {
-        production: {
-            apiUrl: process.env.GHOST_API_URL,
-            contentApiKey: process.env.GHOST_CONTENT_API_KEY,
-        },
-    };
-} finally {
-    const { apiUrl, contentApiKey } =
-        process.env.NODE_ENV === `development`
-            ? ghostConfig.development
-            : ghostConfig.production;
+    try {
+        ghostConfig = require(`./.ghost`);
+    } catch (e) {
+        ghostConfig = {
+            production: {
+                apiUrl: process.env.GHOST_API_URL,
+                contentApiKey: process.env.GHOST_CONTENT_API_KEY,
+            },
+        };
+    } finally {
+        const { apiUrl, contentApiKey } =
+            process.env.NODE_ENV === `development`
+                ? ghostConfig.development
+                : ghostConfig.production;
 
-    if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
+        if (!apiUrl || !contentApiKey || contentApiKey.match(/<key>/)) {
+            throw new Error(
+                `GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`
+            ); // eslint-disable-line
+        }
+    }
+
+    if (
+        process.env.NODE_ENV === `production` &&
+        config.siteUrl === `http://localhost:8000` &&
+        !process.env.SITEURL
+    ) {
         throw new Error(
-            `GHOST_API_URL and GHOST_CONTENT_API_KEY are required to build. Check the README.`
+            `siteUrl can't be localhost and needs to be configured in siteConfig. Check the README.`
         ); // eslint-disable-line
     }
-}
 
-if (
-    process.env.NODE_ENV === `production` &&
-    config.siteUrl === `http://localhost:8000` &&
-    !process.env.SITEURL
-) {
-    throw new Error(
-        `siteUrl can't be localhost and needs to be configured in siteConfig. Check the README.`
-    ); // eslint-disable-line
-}
+    const {
+        NODE_ENV,
+        URL: NETLIFY_SITE_URL = 'https://www.letsreview.co.uk',
+        DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
+        CONTEXT: NETLIFY_ENV = NODE_ENV
+    } = process.env;
+    const isNetlifyProduction = NETLIFY_ENV === 'production';
+    const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
+    /**
+     * This is the place where you can tell Gatsby which plugins to use
+     * and set them up the way you want.
+     *
+     * Further info 👉🏼 https://www.gatsbyjs.org/docs/gatsby-config/
+     *
+     */
 
-const {
-    NODE_ENV,
-    URL: NETLIFY_SITE_URL = 'https://www.letsreview.co.uk',
-    DEPLOY_PRIME_URL: NETLIFY_DEPLOY_URL = NETLIFY_SITE_URL,
-    CONTEXT: NETLIFY_ENV = NODE_ENV
-  } = process.env;
-  const isNetlifyProduction = NETLIFY_ENV === 'production';
-  const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
-/**
- * This is the place where you can tell Gatsby which plugins to use
- * and set them up the way you want.
- *
- * Further info 👉🏼 https://www.gatsbyjs.org/docs/gatsby-config/
- *
- */
+    module.exports = {
+        siteMetadata: {
+            siteUrl: process.env.SITEURL || config.siteUrl,
+        },
+        trailingSlash: 'always',
+        plugins: [
+            /**
+             *  Content Plugins
+             */
+                    /**
 
-module. s = {
-    siteMetadata: {
-        siteUrl: process.env.SITEURL || config.siteUrl,
-    },
-    trailingSlash: 'always',
-    plugins: [
-        /**
-         *  Content Plugins
-         */
-                /**
-
+                {
+                    resolve: `gatsby-source-filesystem`,
+                    options: {
+                        path: path.join(__dirname, `src`, `pages`),
+                        name: `pages`,
+                    },
+                },
+                */
+            // Setup for optimised images.
+            // See https://www.gatsbyjs.org/packages/gatsby-image/
+            /**
             {
                 resolve: `gatsby-source-filesystem`,
                 options: {
-                    path: path.join(__dirname, `src`, `pages`),
-                    name: `pages`,
+                    path: path.join(__dirname, `src`, `images`),
+                    name: `images`,
                 },
             },
+            */
+            `gatsby-plugin-image`,
+            `gatsby-plugin-sharp`,
+            `gatsby-transformer-sharp`,
+            {
+                resolve: `gatsby-source-ghost`,
+                options:
+                    process.env.NODE_ENV === `development`
+                        ? ghostConfig.development
+                        : ghostConfig.production,
+            },
+            /**
+             *  Utility Plugins
              */
-        // Setup for optimised images.
-        // See https://www.gatsbyjs.org/packages/gatsby-image/
-        /**
-        {
-            resolve: `gatsby-source-filesystem`,
-            options: {
-                path: path.join(__dirname, `src`, `images`),
-                name: `images`,
-            },
-        },
-        */
-        `gatsby-plugin-image`,
-        `gatsby-plugin-sharp`,
-        `gatsby-transformer-sharp`,
-        {
-            resolve: `gatsby-source-ghost`,
-            options:
-                process.env.NODE_ENV === `development`
-                    ? ghostConfig.development
-                    : ghostConfig.production,
-        },
-        /**
-         *  Utility Plugins
-         */
-        {
-            resolve: `gatsby-plugin-ghost-manifest`,
-            options: {
-                short_name: config.shortTitle,
-                start_url: `/`,
-                background_color: config.backgroundColor,
-                theme_color: config.themeColor,
-                display: `minimal-ui`,
-                icon: `static/${config.siteIcon}`,
-                legacy: true,
-                query: `
-                {
-                    allGhostSettings {
-                        edges {
-                            node {
-                                title
-                                description
+            {
+                resolve: `gatsby-plugin-ghost-manifest`,
+                options: {
+                    short_name: config.shortTitle,
+                    start_url: `/`,
+                    background_color: config.backgroundColor,
+                    theme_color: config.themeColor,
+                    display: `minimal-ui`,
+                    icon: `static/${config.siteIcon}`,
+                    legacy: true,
+                    query: `
+                    {
+                        allGhostSettings {
+                            edges {
+                                node {
+                                    title
+                                    description
+                                }
                             }
                         }
+                    }
+                `,
+                },
+            },
+            {
+                resolve: 'gatsby-plugin-robots-txt',
+                options: {
+                resolveEnv: () => NETLIFY_ENV,
+                env: {
+                    production: {
+                    policy: [{userAgent: '*'}]
+                    },
+                    'branch-deploy': {
+                    policy: [{userAgent: '*', allow: '/'}],
+                    sitemap: 'https://letsreview.co.uk/sitemap.xml',
+                    host: 'https://letsreview.co.uk'
+                    },
+                    'deploy-preview': {
+                    policy: [{userAgent: '*', disallow: ['/']}],
+                    sitemap: null,
+                    host: null
                     }
                 }
-              `,
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-robots-txt',
-            options: {
-              resolveEnv: () => NETLIFY_ENV,
-              env: {
-                production: {
-                  policy: [{userAgent: '*'}]
-                },
-                'branch-deploy': {
-                  policy: [{userAgent: '*', allow: '/'}],
-                  sitemap: 'https://letsreview.co.uk/sitemap.xml',
-                  host: 'https://letsreview.co.uk'
-                },
-                'deploy-preview': {
-                  policy: [{userAgent: '*', disallow: ['/']}],
-                  sitemap: null,
-                  host: null
                 }
-              }
-            }
-          },
-        {
-            resolve: `gatsby-plugin-feed`,
-            options: {
-                query: `
-                {
-                    allGhostSettings {
-                        edges {
-                            node {
-                                title
-                                description
-                            }
-                        }
-                    }
-                }
-              `,
-                feeds: [generateRSSFeed(config)],
             },
-        },
-        {
-            resolve: `gatsby-plugin-advanced-sitemap`,
-            options: {
-                query: `
-                {
-                    allGhostPost {
-                        edges {
-                            node {
-                                id
-                                slug
-                                updated_at
-                                created_at
-                                feature_image
+            {
+                resolve: `gatsby-plugin-feed`,
+                options: {
+                    query: `
+                    {
+                        allGhostSettings {
+                            edges {
+                                node {
+                                    title
+                                    description
+                                }
                             }
                         }
                     }
-                    allGhostPage {
-                        edges {
-                            node {
-                                id
-                                slug
-                                updated_at
-                                created_at
-                                feature_image
-                            }
-                        }
-                    }
-                    allGhostTag {
-                        edges {
-                            node {
-                                id
-                                slug
-                                feature_image
-                            }
-                        }
-                    }
-                    allGhostAuthor {
-                        edges {
-                            node {
-                                id
-                                slug
-                                profile_image
-                            }
-                        }
-                    }
-                }`,
-                mapping: {
-                    allGhostPost: {
-                        sitemap: `posts`,
-                    },
-                    allGhostTag: {
-                        sitemap: `tags`,
-                    },
-                    allGhostAuthor: {
-                        sitemap: `authors`,
-                    },
-                    allGhostPage: {
-                        sitemap: `pages`,
-                    },
+                `,
+                    feeds: [generateRSSFeed(config)],
                 },
-                exclude: [
-                    `/dev-404-page`,
-                    `/404`,
-                    `/404.html`,
-                    `/offline-plugin-app-shell-fallback`,
-                ],
-                createLinkInHead: true,
-                addUncaughtPages: true,
             },
-        },
-        `gatsby-plugin-catch-links`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-netlify`,
-    ],
-};
+            {
+                resolve: `gatsby-plugin-advanced-sitemap`,
+                options: {
+                    query: `
+                    {
+                        allGhostPost {
+                            edges {
+                                node {
+                                    id
+                                    slug
+                                    updated_at
+                                    created_at
+                                    feature_image
+                                }
+                            }
+                        }
+                        allGhostPage {
+                            edges {
+                                node {
+                                    id
+                                    slug
+                                    updated_at
+                                    created_at
+                                    feature_image
+                                }
+                            }
+                        }
+                        allGhostTag {
+                            edges {
+                                node {
+                                    id
+                                    slug
+                                    feature_image
+                                }
+                            }
+                        }
+                        allGhostAuthor {
+                            edges {
+                                node {
+                                    id
+                                    slug
+                                    profile_image
+                                }
+                            }
+                        }
+                    }`,
+                    mapping: {
+                        allGhostPost: {
+                            sitemap: `posts`,
+                        },
+                        allGhostTag: {
+                            sitemap: `tags`,
+                        },
+                        allGhostAuthor: {
+                            sitemap: `authors`,
+                        },
+                        allGhostPage: {
+                            sitemap: `pages`,
+                        },
+                    },
+                    exclude: [
+                        `/dev-404-page`,
+                        `/404`,
+                        `/404.html`,
+                        `/offline-plugin-app-shell-fallback`,
+                    ],
+                    createLinkInHead: true,
+                    addUncaughtPages: true,
+                },
+            },
+            `gatsby-plugin-catch-links`,
+            `gatsby-plugin-react-helmet`,
+            `gatsby-plugin-offline`,
+            `gatsby-plugin-netlify`,
+        ],
+    };
